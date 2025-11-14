0e1d40e6645206a79e79aac16db9c3f467eef993
Diogo Martino <diogo.martino10@gmail.com>
2025-11-14 20:23:40 +0000

    chore: disable store debug

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -2,7 +2,6 @@ import { Toaster } from '@/components/ui/sonner';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { StoreDebug } from './components/debug/store-debug.tsx';
 import { DialogsProvider } from './components/dialogs/index.tsx';
 import { Routing } from './components/routing/index.tsx';
 import { ServerScreensProvider } from './components/server-screens/index.tsx';
@@ -19,7 +18,7 @@ createRoot(document.getElementById('root')!).render(
     >
       <Toaster />
       <Provider store={store}>
-        <StoreDebug />
+        {/* <StoreDebug /> */}
         <DialogsProvider />
         <ServerScreensProvider />
         <Routing />
